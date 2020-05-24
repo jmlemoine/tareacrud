aabf8a2930e4ab391ac004e4ae3ed2e9d1a90ca0
jmlemoine <jeanmelvinlp27@gmail.com>
2020-05-24 18:49:32 -0400

    Arreglando error 404

--- src/main/java/com/example/demo/TareacrudApplication.java
+++ src/main/java/com/example/demo/TareacrudApplication.java
@@ -2,12 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TareacrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TareacrudApplication.class, args);
+        System.out.println("Klk");
+    }
+
+    @GetMapping("/")
+    public String sayKlk()
+    {
+        return "Klk Jean";
+
     }
 
 }
