fe6977392542d5ed053a1e688c82f7f9849e86db
jmlemoine <jeanmelvinlp27@gmail.com>
2020-05-25 16:43:43 -0400

    Seguimos creando estudiante

--- src/main/java/com/example/demo/Entity/Estudiante.java
+++ src/main/java/com/example/demo/Entity/Estudiante.java
@@ -3,27 +3,27 @@ package com.example.demo.Entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "tEstudiante")
+@Table(name = "t_estudiante")
 public class Estudiante {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "usuarioID")
     private Long id;
-
-    //@Column(name = "usuarioMatricula")
     private int matricula;
-
-    //@Column(name = "usuarioNombre")
     private String nombre;
-
-    //@Column(name = "usuarioTelefono")
     private String telefono;
+
+    private boolean ambosNombres;
+
+    public boolean AmbosNombres(){
+        return !nombre.isEmpty() && !telefono.isEmpty();
+    }
 
     public Estudiante() {
     }
 
     public Estudiante(Long id, int matricula, String nombre, String telefono) {
+        super();
         this.id = id;
         this.matricula = matricula;
         this.nombre = nombre;
@@ -60,5 +60,40 @@ public class Estudiante {
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Estudiante other = (Estudiante) obj;
+
+        if (id == null){
+            if (other.id != null)
+                return false;
+        }
+        else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" +
+                "id=" + id +
+                ", matricula=" + matricula +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
     }
 }
