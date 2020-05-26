2801a4320a2863bf0b2959214a3d88eca32759d5
jmlemoine <jeanmelvinlp27@gmail.com>
2020-05-25 21:15:31 -0400

    Eliminando id y poniendo apellido

--- src/main/java/com/example/demo/Entity/Estudiante.java
+++ src/main/java/com/example/demo/Entity/Estudiante.java
@@ -8,34 +8,19 @@ public class Estudiante {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
     private int matricula;
     private String nombre;
+    private String apellido;
     private String telefono;
-
-    private boolean ambosNombres;
-
-    public boolean AmbosNombres(){
-        return !nombre.isEmpty() && !telefono.isEmpty();
-    }
 
     public Estudiante() {
     }
 
-    public Estudiante(Long id, int matricula, String nombre, String telefono) {
-        super();
-        this.id = id;
+    public Estudiante(int matricula, String nombre, String apellido, String telefono) {
         this.matricula = matricula;
         this.nombre = nombre;
+        this.apellido = apellido;
         this.telefono = telefono;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public int getMatricula() {
@@ -54,6 +39,14 @@ public class Estudiante {
         this.nombre = nombre;
     }
 
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     public String getTelefono() {
         return telefono;
     }
@@ -63,37 +56,14 @@ public class Estudiante {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        Estudiante other = (Estudiante) obj;
-
-        if (id == null){
-            if (other.id != null)
-                return false;
-        }
-        else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-
-        return result;
-    }
-
-    @Override
     public String toString() {
         return "Estudiante{" +
-                "id=" + id +
-                ", matricula=" + matricula +
+                "matricula=" + matricula +
                 ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
                 ", telefono='" + telefono + '\'' +
                 '}';
     }
+
+
 }
